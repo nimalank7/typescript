6ffa57b895949fdc0dc7894010459568088c16a5
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-09-23 17:08:17 +0100

    Creates basic hello world app in node

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) =>{
+    
+    return res.send('Hello World!');
+});
+
+server.listen(4242, () => {
+    console.log('Express Server is running...')
+});
